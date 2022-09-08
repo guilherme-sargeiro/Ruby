59358e07fb5c8a110950224bdfe53cd9ac9fff89
guilherme-sargeiro <gui.sargeiro@outlook.com>
2022-09-07 21:07:17 -0300

    Finalizando com Hashs

--- HandA.rb
+++ HandA.rb
@@ -14,4 +14,25 @@ for value in $values
     puts value **2 
 end
 
+$conteudo = {}
 
+puts "Agora #{$name} Digite o nome de um estado e depois de sua capital, repita esta ação 3 vezes:"
+
+$state1 = gets.chomp.to_s
+$capital1 = gets.chomp.to_s
+
+$state2 = gets.chomp.to_s
+$capital2 = gets.chomp.to_s
+
+$state3 = gets.chomp.to_s
+$capital3 = gets.chomp.to_s
+
+$conteudo = {:"#{$state1}" => "#{$capital1}", :"#{$state2}" => "#{$capital2}", :"#{$state3}" => "#{$capital3}"}
+
+if $conteudo.size == 3 
+    $conteudo.each do |key, value|
+        puts "A chave #{key} possui o valor #{value}"
+    end
+else
+    puts "Você digitiu incorretamente!"
+end
